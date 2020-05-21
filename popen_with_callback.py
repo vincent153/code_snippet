93ce71f23f4be6ab27b6257929cbad7166d75d69
Ting.Vincent <Ting.Vincent@aimobile.com.tw>
2020-05-21 10:20:49 +0800

    add new snippet

---
+++ popen_with_callback.py
@@ -0,0 +1,23 @@
+import threading
+import subprocess
+
+def popenAndCall(onExit, *popenArgs, **popenKWArgs):
+    """
+    Runs a subprocess.Popen, and then calls the function onExit when the
+    subprocess completes.
+
+    Use it exactly the way you'd normally use subprocess.Popen, except include a
+    callable to execute as the first argument. onExit is a callable object, and
+    *popenArgs and **popenKWArgs are simply passed up to subprocess.Popen.
+    """
+    def runInThread(onExit, popenArgs, popenKWArgs):
+        proc = subprocess.Popen(*popenArgs, **popenKWArgs)
+        proc.wait()
+        onExit()
+        return
+
+    thread = threading.Thread(target=runInThread,
+                              args=(onExit, popenArgs, popenKWArgs))
+    thread.start()
+
+    return thread # returns immediately after the thread starts
